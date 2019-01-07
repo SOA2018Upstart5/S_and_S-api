2a4cc8f46a7549fc2e2514c2254d81a61ee0a1b8
szweiwu <sandy.wu@iss.nthu.edu.tw>
2019-01-08 00:32:02 +0800

    gateway

--- app/infrastructure/gateway/unsplash_api.rb
+++ app/infrastructure/gateway/unsplash_api.rb
@@ -5,24 +5,21 @@ require 'http'
 module SeoAssistant
   module OutAPI
     class Unsplash
+      def initialize(unsplash_access_key, keyword_str)
+        @access_key = unsplash_access_key
+        @query = keyword_str
+      end
 
+      def get(url)
+        HTTP.headers(
+          'Authorization' => 'Client-ID ' + @access_key
+        ).get(url)
+      end
 
+      def process
+        path = 'https://api.unsplash.com/' + '/search/photos?query=' + @query
+        pictures_data = get(path).parse
+      end
     end
   end
-end
-
-def initialize(unsplash_access_key, keyword_str)
-  @access_key = unsplash_access_key
-  @query = keyword_str
-end
-
-def get(url)
-  HTTP.headers(
-    'Authorization' => 'Client-ID ' + @access_key
-  ).get(url)
-end
-
-def process
-  path = 'https://api.unsplash.com/' + '/search/photos?query=' + @query
-  pictures_data = get(path).parse
 end
